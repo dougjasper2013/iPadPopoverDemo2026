93b31f9d70772dea60878d9927e81c978578beed
Douglas Jasper <doug.jasper@trios.com>
2026-06-17 08:18:59 -0400

    Initial Commit

---
+++ iPadPopoverDemo2026/iPadPopoverDemo2026App.swift
@@ -0,0 +1,17 @@
+//
+//  iPadPopoverDemo2026App.swift
+//  iPadPopoverDemo2026
+//
+//  Created by Douglas Jasper on 2026-06-17.
+//
+
+import SwiftUI
+
+@main
+struct iPadPopoverDemo2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iPadPopoverDemo2026Tests/iPadPopoverDemo2026Tests.swift
@@ -0,0 +1,19 @@
+//
+//  iPadPopoverDemo2026Tests.swift
+//  iPadPopoverDemo2026Tests
+//
+//  Created by Douglas Jasper on 2026-06-17.
+//
+
+import Testing
+@testable import iPadPopoverDemo2026
+
+struct iPadPopoverDemo2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
